0615bc46fd339d6471010d9263a4b2263486d1ec
devknight216 <andionisie7@gmail.com>
2024-05-16 13:37:53 +0100

    feat: add footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Carousel from './components/Carousel';
+import config from './config';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${config.API_URL}/ext/search?type=technicalTaskCarouselItem&env=dev`);
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      const data = await response.json();
+      setItems(data.reverse()); // Reverse the order of items
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Carousel items={items} /> */}
+      <footer className="bg-[#071c29] text-center p-4">
+        <div className='flex justify-end items-center space-x-2'>
+          <p className="text-white text-md">in partnership with</p>
+          <img 
+            src={config.LOGO_URL} 
+            alt="Logo" 
+            className="mx-auto h-10"
+          />
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  API_URL: 'https://content.inyourarea.co.uk',
+  LOGO_URL: 'https://content.stag.inyourarea.co.uk/ext/asset?url=%2F%2Fimages.ctfassets.net%2F4j8jssfli1wu%2F1DNPm4ngtbUU4MclL4MWrj%2F1be7b068b9952ae84fb470711db3bb85%2FScreenshot_2023-11-13_at_13.14.53.png?h=50'
+};
+
+export default config;
